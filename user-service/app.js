32af86be3b3674d4976c0a95d463393bfeef7a58
Aryan Kumar <128920734+09aryan@users.noreply.github.com>
2024-11-13 21:20:20 +0530

    mongoDb_ulr added

--- user-service/app.js
+++ user-service/app.js
@@ -9,4 +9,9 @@ mongoose.connect('mongodb+srv://24aaryan00:XE1lgXbpxaEs3elB@cluster0.qsbsm.mongo
 
 app.use('/api/users', userRoutes);
 
-app.listen(5000, () => console.log('User Service running on port 5001'));
+// app.listen(5000, () => console.log('User Service running on port 5001'));const app = require('./app');
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
