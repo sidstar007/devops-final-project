dd4eddde26a98920466b419698c6ceb1f95c80a2
Aryan Kumar <128920734+09aryan@users.noreply.github.com>
2024-11-13 21:24:32 +0530

    jenkins issues

--- user-service/app.js
+++ user-service/app.js
@@ -1,17 +1,38 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
+// const app = express();
+// app.use(express.json());
+
+// mongoose.connect('mongodb+srv://24aaryan00:XE1lgXbpxaEs3elB@cluster0.qsbsm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+// app.use('/api/users', userRoutes);
+
+// // app.listen(5000, () => console.log('User Service running on port 5001'));const app = require('./app');
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes');
+
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://24aaryan00:XE1lgXbpxaEs3elB@cluster0.qsbsm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://24aaryan00:XE1lgXbpxaEs3elB@cluster0.qsbsm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use('/api/users', userRoutes);
-
-// app.listen(5000, () => console.log('User Service running on port 5001'));const app = require('./app');
+const app = require('./app');
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// Exporting the app without listening
