bcaef836269429ffbd8f24f8d107098dd4675a10
Siddhant <siddhant.chalke13@gmail.com>
2024-11-14 19:28:48 +0530

    changed gotenv for test

--- user-service/test/user.test.js
+++ user-service/test/user.test.js
@@ -1,4 +1,3 @@
-require('dotenv').config();  // Load environment variables from .env file if present
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../app');
